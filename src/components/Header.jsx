08d462afb932dae45aa62f3b5b749e95690ae5ac
lisanderlee <lisander.lee@gmail.com>
2023-09-29 13:30:52 -0400

    new layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -93,9 +93,9 @@ function MobileNavigation() {
 
 export function Header() {
   return (
-    <header className="py-10">
+    <header className="py-5  bg-white">
       <Container>
-        <nav className="relative z-50 flex justify-between">
+        <nav className=" z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
             <Link href="#" aria-label="Home">
               <Logo className="h-10 w-auto" />
